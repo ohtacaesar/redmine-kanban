e2bded6754377a21ffa60ce91abe58d2600bb290
ohtacaesar <ohtacaesar@gmail.com>
2018-09-23 23:52:56 +0900

    DBにチケットを格納する

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const axiosBase = require('axios');
 
+const mysql = require('mysql');
+
 const REDMINE_API_KEY = process.env.REDMINE_API_KEY;
 
 const axios = axiosBase.create({
@@ -12,12 +14,63 @@ const axios = axiosBase.create({
   }
 });
 
+const connection = mysql.createConnection({
+  host: 'mysql_kanban',
+  user: 'root',
+  password: 'root',
+  database: 'kanban'
+});
+
+connection.query(`
+create table issues (
+  issue_id int not null
+, subject varchar(255) not null
+, description text not null
+
+, primary key(issue_id)
+)
+`);
+
 app.get('/', function (req, res) {
+  res.send(
+      '<!doctype html><html><body><a href="/issues">issues</a><form action="/sync" method="POST"><button type="submit">SYNC</button></form></body></html>');
+});
+
+app.post('/sync', function (req, res) {
   axios.get('/issues.json').then(function (response) {
-    res.json(response.data);
+
+    const issues = response.data.issues;
+    for (let i = 0, l = issues.length; i < l; i++) {
+      const issue = issues[i];
+
+      connection.query(
+          'replace into issues(issue_id, subject, description) values(?)',
+          {
+            'issue_id': issue['id'],
+            'subject': issue['subject'],
+            'description': issue['description'],
+          },
+          function (error, results, fields) {
+            console.log(error);
+            console.log(results);
+            console.log(fields);
+          }
+      );
+
+    }
+
+    res.redirect('/');
   }).catch(function (e) {
     console.log(e);
-    res.send('hellow');
+  });
+});
+
+app.get('/issues', function (req, res) {
+  axios.get('/issues.json').then(function (response) {
+    const issues = response.data.issues;
+    res.json(issues);
+  }).catch(function (e) {
+    console.log(e);
   });
 });
 
---
+++ schema.sql
@@ -0,0 +1,7 @@
+create table issues (
+  issue_id int not null
+, subject varchar(255) not null
+, description text not null
+
+, primary key(issue_id)
+)
